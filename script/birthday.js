a7f64a27de22b34ac2afe0a3208534e4d01e16a5
Vanili <katevanili98@gmail.com>
2022-03-03 10:25:43 +0300

    Added a title to the web application

---
+++ script/birthday.js
@@ -0,0 +1,47 @@
+var gender= [male, female];
+var daysOfTheWeek= [Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday];
+var femaleAkanNames=[Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama]
+var maleAkanNames=[Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame]
+
+
+
+
+if (gender= male){
+    console.log(maleAkanNames)
+} else{
+    console.log(femaleAkanNames)
+}
+
+if(dayOfTheWeek=0){
+    console.log(Sunday)
+} else if(daysOfTheWeek=1){
+    console.log(Monday);
+} else if(daysOfTheWeek=2){
+    console.log(Tuesday);
+} else if(daysOfTheWeek=3){
+    console.log(Wednesday);
+} else if(daysOfTheWeek=4){
+    console.log(Thursday);
+} else if(daysOfTheWeek=5){
+    console.log(Friday);
+} else{
+    console.log(Saturday);
+}
+
+if dayOfTheWeek=Sunday && gender=male{
+    console.log(Kwasi)
+} else if(daysOfTheWeek=Monday && ){
+    console.log(Monday);
+} else if(daysOfTheWeek=2){
+    console.log(Tuesday);
+} else if(daysOfTheWeek=3){
+    console.log(Wednesday);
+} else if(daysOfTheWeek=4){
+    console.log(Thursday);
+} else if(daysOfTheWeek=5){
+    console.log(Friday);
+} else{
+    console.log(Saturday);
+}
+
+prompt("Input date of birth");
