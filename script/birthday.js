80f42698254fc95954c780a2b0d689c2c2d12803
Vanili <katevanili98@gmail.com>
2022-03-03 23:33:30 +0300

    Declared some variables

--- script/birthday.js
+++ script/birthday.js
@@ -1,47 +1,53 @@
-var gender= [male, female];
-var daysOfTheWeek= [Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday];
-var femaleAkanNames=[Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama]
-var maleAkanNames=[Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame]
+function getAkanName(){
+    let yearOfBirth = document.getElementById("year-input").value;
+    let monthOfBirth = document.getElementById("month-input").value;
+    let dayOfBirth = document.getElementById("day-input").value;
+    let genders = document.getElementByName("gender").value;
+    };
 
+    function getGender(){
+        for(let gender of genders){
+            if (gender.checked) {
+                return gender.value;
+            }
+        }
+    };
 
+    let myGenderValue = getGender();
+    console.log(myGenderValue);
 
+    function monthValidator(){
+        if(monthOfBirth < 1 || monthOfBirth > 12){
+            return false;
+        } else {
+            return true;
+        }
+    };
 
-if (gender= male){
-    console.log(maleAkanNames)
-} else{
-    console.log(femaleAkanNames)
-}
+    function dayValidator (){
+        if(monthOfBirth === 2 && Number(yearOfBirth)%4===0){
+            if(dayOfMonth > 28 || dayOfBirth < 1){
+                return false;
+            } else if(monthOfBirth === 2 && dayOfBirth > 29){
+                return false;
+            } else if(monthOfBirth === 2 && dayOfBirth < 1){
+                return false;
+            } else {
+                return true;
+            }
+        }
+    };
 
-if(dayOfTheWeek=0){
-    console.log(Sunday)
-} else if(daysOfTheWeek=1){
-    console.log(Monday);
-} else if(daysOfTheWeek=2){
-    console.log(Tuesday);
-} else if(daysOfTheWeek=3){
-    console.log(Wednesday);
-} else if(daysOfTheWeek=4){
-    console.log(Thursday);
-} else if(daysOfTheWeek=5){
-    console.log(Friday);
-} else{
-    console.log(Saturday);
-}
+    let monthValid = monthValidator();
+    let dayValid = dayValidator();
 
-if dayOfTheWeekSunday && gender=male{
-    console.log(Kwasi)
-} else if(daysOfTheWeek=Monday && ){
-    console.log(Monday);
-} else if(daysOfTheWeek=2){
-    console.log(Tuesday);
-} else if(daysOfTheWeek=3){
-    console.log(Wednesday);
-} else if(daysOfTheWeek=4){
-    console.log(Thursday);
-} else if(daysOfTheWeek=5){
-    console.log(Friday);
-} else{
-    console.log(Saturday);
-}
+    let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
+    ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
 
-prompt("Input date of birth");
+    let daysOfWeek = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday"];
+
+    let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    let maleAkanNames = [ "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+    
